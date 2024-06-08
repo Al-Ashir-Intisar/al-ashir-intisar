11f04a63ebf0f9c103d1e8e08502208d78ab4f68
Al-Ashir-Intisar <iamintisar45@gmail.com>
2024-06-08 12:01:30 -0600

    Home page and cv page functioning

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // script.js
+
+// script.js
+
+document.addEventListener("DOMContentLoaded", function () {
+  var profilePicDiv = document.getElementById("profile-pic");
+  var imgElement = document.createElement("img");
+  imgElement.src = "images/profile_pic.jpg"; // Replace with the path to your image
+  imgElement.alt = "Profile Picture";
+  profilePicDiv.appendChild(imgElement);
+});

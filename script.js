6ed915e7f835cd7605fe598564e1fdc2e56569b1
Al-Ashir-Intisar <iamintisar45@gmail.com>
2024-06-23 14:54:20 -0600

    changed some styles css and created blog post tiles and put biostats final project to papers page

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// script.js
-
-// script.js
+// script for displaying profile picture
 
 document.addEventListener("DOMContentLoaded", function () {
   var profilePicDiv = document.getElementById("profile-pic");
   var imgElement = document.createElement("img");
-  imgElement.src = "images/profile_pic.jpg"; // Replace with the path to your image
+  imgElement.src = "images/profile_pic.jpg"; // Ensure this path is correct
   imgElement.alt = "Profile Picture";
   profilePicDiv.appendChild(imgElement);
+
+  // Making accordion type pdf viewers for papers page
 });
